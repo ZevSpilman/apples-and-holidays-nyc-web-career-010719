ea2360f25f1f44162a77ecd5dc2a7b490f39a64f
Zev Spilman <zevspilman@yahoo.com>
2019-01-06 03:07:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |key, value|
-  answer = key.join(" ")
-  puts answer
+  puts key
 end
 end
 
