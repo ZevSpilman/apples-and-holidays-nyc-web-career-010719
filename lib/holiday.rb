8f47f10cd4ef7f3385c6d365ae320e48239dff1a
Zev Spilman <zevspilman@yahoo.com>
2019-01-06 03:42:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,18 +82,22 @@ holiday_hash.each do |season, data|
 
 
 
-answer = []
-holiday_hash.each do |season, data|
-  data.each do |holiday, value|
-    value.each do |supply|
-     if supply == "BBQ"
-        answer.push(holiday)
-      
-    end
-  end
-end	
-answer
+def all_holidays_with_bbq(holiday_hash)
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+  holiday_hash.each do |key, value|
+    key.map do |holiday, supply|
+      if supply.includes? "BBQ"
+         holiday
+    
+    
+end
+end
+end
+end
 
 
- end
+
+
+
 
